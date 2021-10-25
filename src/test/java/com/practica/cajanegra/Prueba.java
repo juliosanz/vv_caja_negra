3a4f23128fb47d9d0a960faa1d1693afaadd74c9
Luis M. Martin L <l.martinl@alumnos.upm.es>
2021-10-25 18:12:01 +0200

    Agregado clase Prueba (ahora si)

---
+++ src/test/java/com/practica/cajanegra/Prueba.java
@@ -0,0 +1,16 @@
+package com.practica.cajanegra;
+
+import com.cajanegra.SingleLinkedListImpl;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Prueba {
+	
+   @Test
+   public void miTestDeEjemplo(){
+       SingleLinkedListImpl<String> mylist = new SingleLinkedListImpl<String>("A", "B");
+       assertEquals(2,mylist.size());
+   }
+
+}
