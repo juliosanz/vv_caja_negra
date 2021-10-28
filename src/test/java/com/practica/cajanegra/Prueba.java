b25a72f5d684c25e4940efd57684891bdb02e48a
GuillermoFernandezM <guillermo.fernandezm@alumnos.upm.es>
2021-10-28 18:34:11 +0200

    Branch nueva para Julio y Guillermo junto con Clase de pruebas del paquete 2 creada

--- src/test/java/com/practica/cajanegra/Prueba.java
+++ src/test/java/com/practica/cajanegra/Prueba.java
@@ -5,12 +5,14 @@ import com.cajanegra.SingleLinkedListImpl;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 public class Prueba {
 	
-   @Test
-   public void miTestDeEjemplo(){
-       SingleLinkedListImpl<String> mylist = new SingleLinkedListImpl<String>("A", "B");
-       assertEquals(2,mylist.size());
-   }
+	@Test
+	public void miTestDeEjemplo(){
+		SingleLinkedListImpl<String> mylist = new SingleLinkedListImpl<String>("A", "B");
+		assertEquals(2,mylist.size());
+	}
 
+	
 }
---
+++ src/test/java/com/practica/cajanegra/PruebasPaquete2.java
@@ -0,0 +1,96 @@
+package com.practica.cajanegra;
+
+import com.cajanegra.EmptyCollectionException;
+import com.cajanegra.SingleLinkedListImpl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class PruebasPaquete2 {
+	private static SingleLinkedListImpl<String> vacia;
+	private static SingleLinkedListImpl<String> unElemento;
+	private static SingleLinkedListImpl<String> dosElementos;
+	private static SingleLinkedListImpl<String> cincoElementos;
+	
+	@Test
+	@BeforeAll
+	static void setUp() {
+		vacia = new SingleLinkedListImpl<String>();
+		unElemento = new SingleLinkedListImpl<String>("A");
+		dosElementos = new SingleLinkedListImpl<String>("A", "B");
+		cincoElementos = new SingleLinkedListImpl<String>("A", "B", "C", "D", "E");
+	}
+	@Test
+	@BeforeEach
+	public void init() {
+		vacia = new SingleLinkedListImpl<String>();
+		unElemento = new SingleLinkedListImpl<String>("A");
+		dosElementos = new SingleLinkedListImpl<String>("A", "B");
+		cincoElementos = new SingleLinkedListImpl<String>("A", "B", "C", "D", "E");
+	}
+	
+	
+	/*
+	 * 	El siguiente conjunto de tests comprueba
+	 * 	la funcionalidad del método RemoveLast
+	 * 	mediante Robustness for worst case
+	 */
+	@Test
+	public void removeLastVacia() {
+		assertThrows(EmptyCollectionException.class, () -> {vacia.removeLast();});
+	}
+	
+	@Test
+	public void removeLastUnElemento() throws EmptyCollectionException {
+		String prueba = unElemento.removeLast();
+		assertEquals(unElemento.size(), 0); //CUIDADO CON ESTO POR FAVOR ES SIZE NO SABEMOS SI FUNCIONAAAAAA QUE HACEMOS?????
+		assertEquals(prueba, "A");
+	}
+	
+	@Test
+	public void removeLastDosElementos() throws EmptyCollectionException {
+		String prueba = dosElementos.removeLast();
+		assertEquals(unElemento.size(), 1); 
+		assertEquals(prueba, "B");
+	}
+	
+	@Test
+	public void removeLastCincoElementos() throws EmptyCollectionException {
+		String prueba = cincoElementos.removeLast();
+		assertEquals(cincoElementos.size(), 4); 
+		assertEquals(prueba, "E");
+	}
+	
+	/*
+	 * 	El siguiente conjunto de tests comprueba
+	 * 	la funcionalidad del método Size
+	 * 	mediante Robustness for worst case
+	 */
+	
+	@Test
+	public void sizeVacio() {
+		assertEquals(vacia.size(),0);
+	}
+	
+	@Test
+	public void sizeUnElemento() {
+		assertEquals(unElemento.size(),1);
+	}
+	
+	@Test
+	public void sizeDosElementos() {
+		assertEquals(dosElementos.size(),2);
+	}
+	
+	@Test
+	public void sizeCincoElementos() {
+		assertEquals(cincoElementos.size(),5);
+	}
+	
+	
+}
